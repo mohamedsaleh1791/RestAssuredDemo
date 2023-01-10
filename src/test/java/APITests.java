913ddba5a94116feb3b0cd151bce2aeb9a83eb9d
mohamedsaleh1791 <mohamedsaleh1791@gmail.com>
2023-01-10 13:49:04 +0200

    Google map Post Request

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,11 +1,29 @@
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 public class APITests {
     @Test
     void test(){
-        Response response= RestAssured.get("http://httpbin.org/json");
-        System.out.println(response.statusCode());
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": -38.383494,\n" +
+                        "    \"lng\": 33.427362\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"Test\",\n" +
+                        "  \"phone_number\": \"(+12) 123456789\",\n" +
+                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://Test.com\",\n" +
+                        "  \"language\": \"English\"\n" +
+                        "}\n").when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
     }
 }
